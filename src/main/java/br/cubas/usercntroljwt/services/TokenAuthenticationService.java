f25536164d9020f0c7e19a2224a5158734b97096
carloscubas <carlos.cubas@gmail.com>
2018-01-05 21:25:32 -0200

    inclusão de um método de exemplo para parse do token

--- src/main/java/br/cubas/usercntroljwt/services/TokenAuthenticationService.java
+++ src/main/java/br/cubas/usercntroljwt/services/TokenAuthenticationService.java
@@ -18,17 +18,18 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class TokenAuthenticationService {
 
-    private static final long EXPIRATIONTIME = 864000000;
-    private static final String SECRET = "MySecreteApp";
-    private static final String TOKEN_PREFIX = "Bearer";
-    private static final String HEADER_STRING = "Authorization";
-    
+	private static final long EXPIRATIONTIME = 864000000;
+	private static final String SECRET = "MySecreteApp";
+	private static final String TOKEN_PREFIX = "Bearer";
+	private static final String HEADER_STRING = "Authorization";
+
 	public static String addAuthentication(Authentication auth) {
 		Claims claims = Jwts.claims().setSubject(auth.getName());
 		claims.put("roles", auth.getAuthorities());
@@ -41,36 +42,31 @@ public class TokenAuthenticationService {
 		return token;
 	}
 
-    public static void addAuthentication(HttpServletResponse res, Authentication auth) {
-    	
-   	 Claims claims = Jwts.claims().setSubject(auth.getName());
-     claims.put("roles", auth.getAuthorities());
-    	
-        String JWT = Jwts.builder()
-                .setClaims(claims)
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
-                .signWith(SignatureAlgorithm.HS512, SECRET)
-                .compact();
+	public static void addAuthentication(HttpServletResponse res, Authentication auth) {
 
-        String token = TOKEN_PREFIX + " " + JWT;
-        res.addHeader(HEADER_STRING, token);
+		Claims claims = Jwts.claims().setSubject(auth.getName());
+		claims.put("roles", auth.getAuthorities());
 
-        try {
-            res.getOutputStream().print(token);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		String JWT = Jwts.builder().setClaims(claims)
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+				.signWith(SignatureAlgorithm.HS512, SECRET).compact();
 
-    public static Authentication getByToken(String token) {
-    	
-    	Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-    	
-    	 Claims claims = Jwts.parser()
-                 .setSigningKey(SECRET)
-                 .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
-                 .getBody();
-    	 
+		String token = TOKEN_PREFIX + " " + JWT;
+		res.addHeader(HEADER_STRING, token);
+
+		try {
+			res.getOutputStream().print(token);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Authentication getByToken(String token) {
+
+		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+		Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token.replace(TOKEN_PREFIX, "")).getBody();
+
 		List<String> roles = (ArrayList) claims.get("roles");
 		for (Object role : roles) {
 			HashMap<String, String> map = (HashMap<String, String>) role;
@@ -81,21 +77,54 @@ public class TokenAuthenticationService {
 			}
 
 		}
-    	
-        String user = Jwts.parser()
-                .setSigningKey(SECRET)
-                .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
-                .getBody()
-                .getSubject();
 
-        return user != null ? new UsernamePasswordAuthenticationToken(user, null, grantedAuthorities) : null;
-    }
+		String user = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token.replace(TOKEN_PREFIX, "")).getBody()
+				.getSubject();
 
-    public static Authentication getAuthentication(HttpServletRequest request) {
-        String token = request.getHeader(HEADER_STRING);
-        if (token != null) {
-            return getByToken(token);
-        }
-        return null;
-    }
+		return user != null ? new UsernamePasswordAuthenticationToken(user, null, grantedAuthorities) : null;
+	}
+
+	public static Authentication getAuthentication(HttpServletRequest request) {
+		String token = request.getHeader(HEADER_STRING);
+		if (token != null) {
+			return getByToken(token);
+		}
+		return null;
+	}
+
+	/**
+	 * Exemplo de como ler as informacões de um token
+	 * @param token
+	 * @return
+	 */
+	public static Authentication getInformationByToken(String token) {
+
+		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+		Jws<Claims> claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token.replace(TOKEN_PREFIX, ""));
+
+		List<String> roles = (ArrayList) claims.getBody().get("roles");
+
+		String user = claims.getBody().getSubject();
+		
+		String signature = claims.getSignature();
+
+		Date dataExpiracao = claims.getBody().getExpiration();
+
+		for (Object role : roles) {
+
+			HashMap<String, String> map = (HashMap<String, String>) role;
+			List<String> list = new ArrayList<>();
+			list.addAll(map.values());
+			for (String item : list) {
+
+				grantedAuthorities.add(new SimpleGrantedAuthority(item));
+
+			}
+
+		}
+
+		return user != null ? new UsernamePasswordAuthenticationToken(user, null, grantedAuthorities) : null;
+
+	}
 }
